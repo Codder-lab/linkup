e94137515827a8350b9d0c08522a6fbec79d4a05
suyashpotdar <suyashpotdar03@gmail.com>
2024-09-11 14:39:02 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/home";
+import LoginPage from "scenes/login";
+import ProfilePage from "scenes/profilePage";
+
+function App() {
+  return <div className="app"></div>;
+}
+
+export default App;
